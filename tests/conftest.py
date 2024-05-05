914442666a0dea98f822d6342476eeb680b914aa
Elmin Gulamov <g.elmn@yandex.ru>
2024-05-05 14:53:35 +0400

    gitignore

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,44 @@
 import pytest
-from selene import browser
+from selene import Browser, Config
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from utils import attach
+import os
+
+DEFAULT_BROWSER_VERSION = "100.0"
 
 
-@pytest.fixture(scope='function', autouse=True)
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='function')
 def configuration(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
+
+    browser = Browser(Config(driver))
 
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
