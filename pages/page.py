1841a6287940271b6b233edeba083cd79d6c8e9a
Elmin Gulamov <g.elmn@yandex.ru>
2024-05-12 20:29:31 +0400

    add

--- pages/page.py
+++ pages/page.py
@@ -1,29 +1,35 @@
-from selene import have, command
+import allure
+from selene import command
 from selene import browser, have
 
 
+@allure.tag("WEB")
 class MainPage:
-
+    @allure.title('Открытие страницы')
     def open(self):
         browser.open('/')
 
+    @allure.title('Выбор города')
     def fill_city(self):
         browser.element('.supernova-navi-item_area-switcher-button').click()
         browser.element('[href="https://saratov.hh.ru/?customDomain=1"]').click()
 
+    @allure.title('Поиск компании')
     def search_company(self):
-        browser.element('#a11y-search-input').type('Юmoney').press_enter()
+        browser.element('#a11y-search-input').type('Qiwi').press_enter()
         browser.element('[data-qa="bloko-modal-close"]').click()
         browser.element('[data-hh-tab-id="employersList"]').click()
-        browser.element('[href="/employer/655542"]').click()
+        browser.element('[href="/employer/3125"]').click()
 
+    @allure.title('Поиск резюме')
     def fill_resume(self):
         browser.element('#a11y-search-input').type('Специалист Яндекс').press_enter()
         browser.element('[data-qa="bloko-modal-close"]').click()
         browser.element('[data-hh-tab-id="resumeSearch"]').click()
         browser.element('[data-qa="serp-item__title"]').click()
 
-    def search_job(self):
+    @allure.title('Поиск работы')
+    def fill_job(self):
         browser.element('#a11y-search-input').type('Python').press_enter()
         browser.element('[data-qa="bloko-modal-close"]').click()
         browser.element('[name=part_time][value=start_after_sixteen]+span').click()
@@ -31,23 +37,26 @@ class MainPage:
         browser.element('[class=bloko-checkbox__input][value="124"]+span').perform(command.js.scroll_into_view).click()
         browser.element('[name=education][value="not_required_or_not_specified"]+span').click()
 
+    @allure.title('Проверка выбора города')
     def should_have_city(self):
-        browser.element('.supernova-navi-item_area-switcher-button').click()
-        browser.element('[href="https://saratov.hh.ru/?customDomain=1"]').click()
         browser.element('[data-qa="mainmenu_areaSwitcher"]').should(have.text('Саратов'))
 
+    @allure.title('Проверка информации')
     def should_have_company(self):
-        browser.element('#a11y-search-input').type('Юmoney').press_enter()
-        browser.element('[data-qa="bloko-modal-close"]').click()
-        browser.element('[data-hh-tab-id="employersList"]').click()
-        browser.element('[data-qa="employers-list-company-list"]').should(have.text('ЮMoney'))
+        browser.element('.g-user-content').should(have.text('QIWI'))
+
+    def should_have_job(self):
+        pass
+
+
+    def should_have_resume(self):
+        pass
 
     def select_city(self):
         self.open()
         self.fill_city()
 
     def check_city(self):
-        self.open()
         self.should_have_city()
 
     def find_company(self):
@@ -55,9 +64,17 @@ class MainPage:
         self.search_company()
 
     def check_company(self):
-        self.open()
         self.should_have_company()
 
     def search_resume(self):
         self.open()
         self.fill_resume()
+
+    def check_resume(self):
+        self.should_have_resume()
+
+    def search_job(self):
+        self.open()
+        self.fill_job()
+    def check_job(self):
+        self.should_have_job()
--- tests/test_hh.py
+++ tests/test_hh.py
@@ -1,9 +1,28 @@
+import allure
+
 from pages.page import MainPage
 
 
+@allure.tag("WEB")
 def test_search_city():
     m = MainPage()
     m.select_city()
     m.check_city()
 
 
+def test_search_company():
+    m = MainPage()
+    m.find_company()
+    m.check_company()
+
+
+def test_search_resume():
+    m = MainPage()
+    m.search_resume()
+    m.check_resume()
+
+
+def test_search_job():
+    m = MainPage()
+    m.search_job()
+    m.check_job()
